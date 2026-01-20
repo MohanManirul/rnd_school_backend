acdd146e9331e3d9051545b5108fef9102a2a039
Mohan Manirul <fiforeg@gmail.com>
2026-01-20 22:40:40 +0600

    ecom backend ok

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -26,6 +27,10 @@ class User extends Authenticatable
         'otp'
     ];
 
+    public function profile(): HasOne
+    {
+        return $this->hasOne(CustomerProfile::class);
+    }
    
     /**
      * The attributes that should be hidden for serialization.
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,13 @@ use App\Http\Controllers\API\TaskController;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\Backend\ModuleController;
 use App\Http\Controllers\Backend\UserModule\DashboardController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\PolicyController;
+use App\Http\Controllers\ProductController;
+use App\Http\Middleware\TokenAuthenticate;
 
 Route::prefix('v1')->group(function () {
 
@@ -76,3 +83,76 @@ Route::prefix('/admindashboard')
         // 🟢 Include additional route files
         require_once base_path('routes/backend/shift.php');
     });
+
+    Route::get('/', [HomeController::class, 'HomePage']);
+    Route::get('/by-category', [CategoryController::class, 'ByCategoryPage']);
+    Route::get('/by-brand', [BrandController::class, 'ByBrandPage']);
+    Route::get('/policy', [PolicyController::class, 'PolicyPage']);
+    Route::get('/details', [ProductController::class, 'Details']);
+    Route::get('/login', [UserController::class, 'LoginPage']);
+    Route::get('/verify', [UserController::class, 'VerifyPage']);
+    Route::get('/wish', [ProductController::class, 'WishList']);
+    Route::get('/cart', [ProductController::class, 'CartListPage']);
+    Route::get('/profile', [ProfileController::class, 'ProfilePage']);
+
+
+    // Brand List
+    Route::get('/BrandList', [BrandController::class, 'BrandList']);
+    // Category List
+    Route::get('/CategoryList', [CategoryController::class, 'CategoryList']);
+    // Product List
+    Route::get('/ListProductByCategory/{id}', [ProductController::class, 'ListProductByCategory']);
+    Route::get('/ListProductByBrand/{id}', [ProductController::class, 'ListProductByBrand']);
+    Route::get('/ListProductByRemark/{remark}', [ProductController::class, 'ListProductByRemark']);
+    // Slider
+    Route::get('/ListProductSlider', [ProductController::class, 'ListProductSlider']);
+    // Product Details
+    Route::get('/ProductDetailsById/{id}', [ProductController::class, 'ProductDetailsById']);
+    Route::get('/ListReviewByProduct/{product_id}', [ProductController::class, 'ListReviewByProduct']);
+    //policy
+    Route::get("/PolicyByType/{type}",[PolicyController::class,'PolicyByType']);
+
+
+
+    // User Auth
+    Route::get('/UserLogin/{UserEmail}', [UserController::class, 'UserLogin']);
+    Route::get('/VerifyLogin/{UserEmail}/{OTP}', [UserController::class, 'VerifyLogin']);
+    Route::get('/logout',[UserController::class,'UserLogout']);
+
+
+    // User Profile
+    Route::post('/CreateProfile', [ProfileController::class, 'CreateProfile'])->middleware([TokenAuthenticate::class]);
+    Route::get('/ReadProfile', [ProfileController::class, 'ReadProfile'])->middleware([TokenAuthenticate::class]);
+
+
+    // Product Review
+    Route::post('/CreateProductReview', [ProductController::class, 'CreateProductReview'])->middleware([TokenAuthenticate::class]);
+
+
+
+    // Product Wish
+    Route::get('/ProductWishList', [ProductController::class, 'ProductWishList'])->middleware([TokenAuthenticate::class]);
+    Route::get('/CreateWishList/{product_id}', [ProductController::class, 'CreateWishList'])->middleware([TokenAuthenticate::class]);
+    Route::get('/RemoveWishList/{product_id}', [ProductController::class, 'RemoveWishList'])->middleware([TokenAuthenticate::class]);
+
+
+
+    // Product Cart
+    Route::post('/CreateCartList', [ProductController::class, 'CreateCartList'])->middleware([TokenAuthenticate::class]);
+    Route::get('/CartList', [ProductController::class, 'CartList'])->middleware([TokenAuthenticate::class]);
+    Route::get('/DeleteCartList/{product_id}', [ProductController::class, 'DeleteCartList'])->middleware([TokenAuthenticate::class]);
+
+
+
+
+    // Invoice and payment
+    Route::get("/InvoiceCreate",[InvoiceController::class,'InvoiceCreate'])->middleware([TokenAuthenticate::class]);
+    Route::get("/InvoiceList",[InvoiceController::class,'InvoiceList'])->middleware([TokenAuthenticate::class]);
+    Route::get("/InvoiceProductList/{invoice_id}",[InvoiceController::class,'InvoiceProductList'])->middleware([TokenAuthenticate::class]);
+
+
+    //payment
+    Route::post("/PaymentSuccess",[InvoiceController::class,'PaymentSuccess']);
+    Route::post("/PaymentCancel",[InvoiceController::class,'PaymentCancel']);
+    Route::post("/PaymentFail",[InvoiceController::class,'PaymentFail']);
+    Route::post("/PaymentIPN",[InvoiceController::class,'PaymentIPN']);
